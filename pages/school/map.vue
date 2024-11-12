01bf643e5cfb3f7060a00d9ce984ae3a1585b4f2
Airmole <admin@airmole.cn>
2024-11-12 19:55:03 +0800

    关闭 uni 统计，优化校园地图功能

--- pages/school/map.vue
+++ pages/school/map.vue
@@ -10,24 +10,31 @@
 		</scroll-view>
 		<view class="no-scrool flex justify-center align-center" :style="{ height: `${pageHeight-50}px`,width: '100%' }">
 			<map
-				:style="{ height: `${pageHeight}px`,width: '100%' }"
+				:style="{ height: `${pageHeight-60}px`,width: '100%' }"
 				:longitude="defaultCenter[0]"
 				:latitude="defaultCenter[1]"
 				:scale="18"
 				:show-location="true"
-				:show-compass="true"
 				:markers="markers"
 				:include-points="points"
 				@markertap="markerTap"
 				@regionchange="regionChange"
 			></map>
 			
-			<view class="bg-white padding-xs round map-tool flex flex-direction" :style="`bottom: ${foldPoiCardArea?'10vh':'43vh'};`">
-				<view class="margin-tb-xs" @tap="previewMapPic">
-					<image src="https://r2.airmole.net/images/weapp/navIcon/staticMap.png" mode="aspectFill" class="tool-icon"></image>
+			<view class="bg-white padding-xs round map-tool flex flex-direction justify-center text-center" :style="`bottom: ${foldPoiCardArea?'10vh':'43vh'};`">
+				<view class="margin-tb-xs flex flex-direction" @tap.stop="goFullviewMap">
+					<button class="cu-btn cuIcon tool-icon fullview-btn">
+						<text class="iconfont icon-fullview text-white text-bold" style="font-size: 50rpx;"></text>
+					</button>
+					<text class="text-xs">全景地图</text>
 				</view>
-				<view class="margin-tb-xs" @tap="resetMapCenter">
+				<view class="margin-tb-xs flex flex-direction" @tap.stop="previewMapPic">
+					<image src="https://r2.airmole.net/images/weapp/navIcon/staticMap.png" mode="aspectFill" class="tool-icon"></image>
+					<text class="text-xs">平面地图</text>
+				</view>
+				<view class="margin-tb-xs flex flex-direction" @tap.stop="resetMapCenter">
 					<image src="https://r2.airmole.net/images/weapp/navIcon/moveSchool.png" mode="aspectFill" class="tool-icon"></image>
+					<text class="text-xs">回到学校</text>
 				</view>
 			</view>
 			
@@ -43,10 +50,11 @@
 				<scroll-view v-if="!foldPoiCardArea" :scroll-top="scrollTop" scroll-y scroll-with-animation class="padding-tb-sm poi-card-content">
 					<view @tap="placeCheck(item)" :class="`flex padding-sm radius ${item.id==checkedPlaceId?'bg-blue light':''}`" v-for="(item, index) in currentTabPoi" :key="index">
 						<view class="flex-twice">
-							<view class="cu-avatar lg radius bg-gradual-blue"><text class="text-sm">{{item.title}}</text></view>
+							<view v-if="item.image" @tap.stop="previewImg(item.image)" class="cu-avatar lg radius" :style="`background-image:url(${item.image});`"></view>
+							<view v-else class="cu-avatar lg radius bg-gradual-blue text-cut"><text class="text-xs">{{item.title}}</text></view>
 						</view>
 						<view class="flex-treble text-xl text-left flex align-center"><view>{{item.title}}</view></view>
-						<view class="flex-sub flex align-center text-xl"><view @tap.stop="openLocation(item)" class="bg-gradual-blue padding-xs round"><text class="text-white cuIcon-locationfill"></text></view></view>
+						<view class="flex-sub flex align-center text-xl"><view @tap.stop="openLocation(item)" class="bg-gradual-blue padding-xs padding-lr-sm round"><text class="text-white cuIcon-locationfill"></text></view></view>
 					</view>
 				</scroll-view>
 			</view>
@@ -62,7 +70,7 @@
 		data() {
 			return {
 				pageHeight: 0,
-				defaultCenter: [117.395798, 39.5430],
+				defaultCenter: [117.392825, 39.542733],
 				currentTab: 0,
 				scrollLeft: 0,
 				scrollTop: 0,
@@ -155,12 +163,71 @@
 				this.checkedPlaceId = e.detail.markerId
 				const index = this.currentTabPoi.findIndex(place => Number(place.id) === Number(e.detail.markerId))
 				if (index >= 0) this.scrollTop = (index - 1) * 60
+				const place = this.currentTabPoi[index]
+				const currentCategory = this.categories[this.currentTab]
+				const markers = []
+				for (let poi of this.allPoi) {
+					if (poi.category !== currentCategory) continue
+					let item = {
+					  id: Number(poi.id),
+					  latitude: Number(poi.latitude),
+					  longitude: Number(poi.longitude),
+					  iconPath: `https://r2.airmole.net/images/weapp/navIcon/${poi.icon}.png`,
+					  width: 30,
+					  height: 32,
+					  callout: {
+						content: poi.title,
+						display: 'BYCLICK'
+					  }
+					}
+					if (Number(place.id) === Number(poi.id)) {
+						item.label = {
+							content: place.title,
+							color: '#ffffff',
+							fontSize: 16,
+							bgColor: '#3C87ED',
+							padding: 4
+						}
+					}
+					markers.push(item)
+				}
+				this.defaultCenter = [Number(place.longitude), Number(place.latitude)]
+				this.markers = markers
+				this.foldPoiCardArea = true
 			},
 			placeCheck (place) {
 				this.checkedPlaceId = Number(place.id)
 				const index = this.currentTabPoi.findIndex(item => Number(item.id) === Number(place.id))
 				if (index >= 0) this.scrollTop = (index - 1) * 60
+				const currentCategory = this.categories[this.currentTab]
+				const markers = []
+				for (let poi of this.allPoi) {
+					if (poi.category !== currentCategory) continue
+					let item = {
+					  id: Number(poi.id),
+					  latitude: Number(poi.latitude),
+					  longitude: Number(poi.longitude),
+					  iconPath: `https://r2.airmole.net/images/weapp/navIcon/${poi.icon}.png`,
+					  width: 30,
+					  height: 32,
+					  callout: {
+						content: poi.title,
+						display: 'BYCLICK'
+					  }
+					}
+					if (Number(place.id) === Number(poi.id)) {
+						item.label = {
+							content: place.title,
+							color: '#ffffff',
+							fontSize: 16,
+							bgColor: '#3C87ED',
+							padding: 4
+						}
+					}
+					markers.push(item)
+				}
 				this.defaultCenter = [Number(place.longitude), Number(place.latitude)]
+				this.markers = markers
 				this.foldPoiCardArea = true
 			},
 			regionChange (e) {
@@ -178,11 +245,40 @@
 			},
 			previewMapPic () {
 				uni.previewImage({
-					urls: ['https://r2.airmole.net/i/2024/11/11/wg3ar-dv.png']
+					urls: ['https://r2.airmole.net/i/2024/11/12/vbmpa-2i.png']
+				})
+			},
+			previewImg (url) {
+				uni.previewImage({
+					urls: [url]
 				})
 			},
 			resetMapCenter () {
-				this.defaultCenter = [117.395798, 39.5430]
+				this.defaultCenter = [117.392825, 39.542733]
+			},
+			goFullviewMap () {
+				const url = 'https://720yun.com/vr/c80jOsmvzy2'
+				// #ifdef MP-WEIXIN
+				uni.setClipboardData({
+					data: url,
+					success() {
+						uni.showToast({
+							title: '链接已复制到粘贴板，请打开浏览器后粘贴访问！',
+							icon: 'none'
+						})
+					}
+				})
+				// #endif
+				
+				// #ifdef H5
+				window.location.href = url
+				// #endif
+			}
+		},
+		onShareAppMessage(res) {
+			return {
+			  title: '校园地图 - 贝壳小盒子',
+			  path: '/pages/school/map'
 			}
 		}
 	}
@@ -209,5 +305,10 @@
 	.tool-icon {
 		width: 80upx;
 		height: 80upx;
+	}
+	.fullview-btn {
+		background-color: #4594D5 !important;
+		height: 80rpx !important;
+		width: 80rpx !important;
 	}
 </style>
